6d067cc4bd204c896f8835f28c39bfbb5a67c136
aljosa <aljosa.mrak@gmail>
2021-11-21 16:15:44 +0100

    Another minor configuration changes/fixes/refactorings

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const appStyles = require("./App.css");
+const appStyles = require("./App.scss");
 
 interface IUserPrefs {
   colorful: boolean;
--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
-require("./index.css");
+require("./index.scss");
 
 ReactDOM.render(
   <App />,
