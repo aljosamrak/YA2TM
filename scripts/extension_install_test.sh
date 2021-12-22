f0aa64c40569c3e1b0dd628035ab45b9e8661012
aljosa <aljosa.mrak@gmail>
2021-12-22 12:54:29 +0100

    Add extension installation test step

---
+++ scripts/extension_install_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+EXTENSION_SEARCH_PATTERN="Extension error: "
+RUN_DIR="/tmp/$RANDOM"
+CHROME_PATH=$(which google-chrome)
+
+# Load extension to Chrome and run Chrome as fresh start
+"$CHROME_PATH"\
+  --user-data-dir="$RUN_DIR" \
+  --load-extension=build/ \
+  --no-first-run \
+  --enable-logging --v=1 \
+  &
+
+# Wait one second and kill the chrome process
+sleep 1
+kill "$!"
+
+# Grep the logs for any extension errors
+errors=$(grep "$EXTENSION_SEARCH_PATTERN" "$RUN_DIR/chrome_debug.log")
+
+# Exit with error if any extension errors present
+if [[ $errors ]]; then
+  echo "${errors##*$EXTENSION_SEARCH_PATTERN}"
+  exit 1
+fi
