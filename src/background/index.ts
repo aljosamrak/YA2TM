3e80e7940ed91fadab3f3b19afb1a6bc26170b51
aljosa <aljosa.mrak@gmail>
2022-01-06 22:36:26 +0100

    Add additional callbacks to the background script

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,18 +1,145 @@
 import { insert_records } from '../database/Database'
+import Tab = chrome.tabs.Tab
+import TabActiveInfo = chrome.tabs.TabActiveInfo
+import Window = chrome.windows.Window
+import WindowEventFilter = chrome.windows.WindowEventFilter
 
-chrome.tabs.onCreated.addListener((tab) => {
+let activeTabs: TabActiveInfo[] = []
+
+async function setupListeners() {
+  // Remove listeners
+  chrome.tabs.onCreated.removeListener(tabAdded)
+  chrome.tabs.onRemoved.removeListener(tabRemoved)
+  chrome.tabs.onReplaced.removeListener(tabRemoved)
+  chrome.tabs.onDetached.removeListener(tabRemoved)
+  chrome.tabs.onAttached.removeListener(tabRemoved)
+  chrome.tabs.onActivated.removeListener(tabActiveChanged)
+  chrome.tabs.onMoved.removeListener(tabRemoved)
+  chrome.windows.onFocusChanged.removeListener(windowFocus)
+  chrome.windows.onCreated.removeListener(windowCreated)
+  chrome.windows.onRemoved.removeListener(windowRemoved)
+
+  // Add listeners
+  chrome.tabs.onCreated.addListener(tabAdded)
+  chrome.tabs.onUpdated.addListener(tabRemoved)
+  chrome.tabs.onRemoved.addListener(tabRemoved)
+  chrome.tabs.onReplaced.addListener(tabRemoved)
+  chrome.tabs.onDetached.addListener(tabRemoved)
+  chrome.tabs.onAttached.addListener(tabRemoved)
+  chrome.tabs.onActivated.addListener(tabActiveChanged)
+  chrome.tabs.onMoved.addListener(tabRemoved)
+  chrome.windows.onFocusChanged.addListener(windowFocus)
+  chrome.windows.onCreated.addListener(windowCreated)
+  chrome.windows.onRemoved.addListener(windowRemoved)
+  updateTabCount()
+}
+
+async function tabAdded(tab: Tab) {
   add('opened', tab)
-})
+  updateTabCount()
+}
 
-chrome.tabs.onRemoved.addListener((tab) => {
+async function tabRemoved(tabId: number) {
   add('closed', undefined)
-})
+}
+
+function tabActiveChanged(tab: TabActiveInfo) {
+  if (!!tab && !!tab.tabId) {
+    let i
+    if (!activeTabs) {
+      activeTabs = []
+    }
+    if (!!activeTabs && activeTabs.length > 0) {
+      const lastActive = activeTabs[activeTabs.length - 1]
+      if (!!lastActive && lastActive.tabId === tab.tabId && lastActive.windowId === tab.windowId) {
+        return
+      }
+    }
+    while (activeTabs.length > 20) {
+      activeTabs.shift()
+    }
+    for (i = activeTabs.length - 1; i >= 0; i--) {
+      if (activeTabs[i].tabId === tab.tabId) {
+        activeTabs.splice(i, 1)
+      }
+    }
+    activeTabs.push(tab)
+  }
+  updateTabCount()
+}
+
+function windowFocus(windowId: number) {
+  try {
+    if (!!windowId) {
+      windowActive(windowId)
+      hideWindows(windowId)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function windowCreated(window: Window, filter?: WindowEventFilter | undefined) {
+  try {
+    if (!!window && !!window.id) {
+      windowActive(window.id)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function windowRemoved(windowId: number) {
+  try {
+    if (!!windowId) {
+      windowActive(windowId)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function hideWindows(windowId: number) {
+  if (navigator.userAgent.search('Firefox') > -1) {
+    return
+  }
+
+  if (!windowId || windowId < 0) {
+    return
+  } else {
+    if (localStorageAvailable()) {
+      if (typeof localStorage.hideWindows === 'undefined') {
+        localStorage.hideWindows = '0'
+      }
+      if (localStorage.hideWindows === '0') {
+        return
+      }
+    } else {
+      console.log('no local storage')
+      return
+    }
+  }
+}
+
+function windowActive(windowId: number) {
+  if (windowId < 0) {
+    return
+  }
+  let windows = JSON.parse(localStorage.windowAge)
+  if (!(windows instanceof Array)) {
+    windows = []
+  }
+  if (windows.indexOf(windowId) > -1) {
+    windows.splice(windows.indexOf(windowId), 1)
+  }
+  windows.unshift(windowId)
+  localStorage.windowAge = JSON.stringify(windows)
+}
 
 async function add(operation: string, tab?: chrome.tabs.Tab) {
   // Query opened tabs and windows
   const timeNow = Date.now()
-  const windows = await chrome.windows.getAll()
-  const tabs = await chrome.tabs.query({})
+  const [windows, tabs] = await Promise.all([chrome.windows.getAll(), chrome.tabs.query({})])
 
   chrome.action.setBadgeText({ text: tabs.length.toString() })
 
@@ -25,6 +152,37 @@ async function add(operation: string, tab?: chrome.tabs.Tab) {
   })
 }
 
-// Set tabs count on start
-chrome.tabs.query({}).then((tabs) =>
-  chrome.action.setBadgeText({ text: tabs.length.toString() }))
+async function updateTabCount() {
+  let run = true
+  if (localStorageAvailable()) {
+    if (typeof localStorage.badge === 'undefined') {
+      localStorage.badge = '1'
+    }
+    if (localStorage.badge === '0') {
+      run = false
+    }
+  }
+
+  if (run) {
+    // Set tabs count on start
+    chrome.tabs.query({}).then((tabs) => {
+        chrome.action.setBadgeText({ text: tabs.length.toString() })
+      },
+    )
+  } else {
+    await chrome.action.setBadgeText({ text: '' })
+  }
+}
+
+function localStorageAvailable() {
+  const test = 'test'
+  try {
+    localStorage.setItem(test, test)
+    localStorage.removeItem(test)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+setupListeners()
