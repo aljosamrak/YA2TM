54076f2c0c6e29e87a2477490e882f6a2d908994
aljosa <aljosa.mrak@gmail>
2022-01-22 21:28:48 +0100

    Fix wrongly referenced this in TabController

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,12 +1,6 @@
-import { Database } from '../storage/Database'
 import { container } from '../inversify/inversify.config'
-import { BadgeController } from './BadgeController'
 import { TYPES } from '../inversify/types'
 import { TabController } from '../controller/tab/TabController'
 
-const database = container.get<Database>(TYPES.DatabaseService)
-const badgeController = container.get<BadgeController>(TYPES.BadgeController)
-const tabController = container.get<TabController>(TYPES.TabController)
-
-tabController.setupListeners()
-
+// Create Tab controller
+container.get<TabController>(TYPES.TabController)
--- src/controller/tab/TabController.ts
+++ src/controller/tab/TabController.ts
@@ -21,59 +21,45 @@ class TabController {
     @inject(TYPES.WindowData) private windowData: WindowData,
     @inject(TYPES.LocalStorageService) private localStorage: LocalStorage,
     @inject(TYPES.DatabaseService) private database: Database,
-    @inject(TYPES.BadgeController) private badgeController: BadgeController
-  ) { }
-
-  async setupListeners() {
-    // Remove listeners
-    chrome.tabs.onCreated.removeListener(this.tabCreated)
-    chrome.tabs.onUpdated.removeListener(this.tabUpdated)
-    chrome.tabs.onRemoved.removeListener(this.tabRemoved)
-    chrome.tabs.onReplaced.removeListener(this.tabRemoved)
-    chrome.tabs.onDetached.removeListener(this.tabRemoved)
-    chrome.tabs.onAttached.removeListener(this.tabRemoved)
-    chrome.tabs.onMoved.removeListener(this.tabRemoved)
-    chrome.windows.onCreated.removeListener(this.windowCreated)
-    chrome.windows.onRemoved.removeListener(this.windowRemoved)
-
-    // Add listeners
-    chrome.tabs.onCreated.addListener(this.tabCreated)
-    chrome.tabs.onUpdated.addListener(this.tabUpdated)
-    chrome.tabs.onRemoved.addListener(this.tabRemoved)
-    chrome.tabs.onReplaced.addListener(this.tabRemoved)
-    chrome.tabs.onDetached.addListener(this.tabRemoved)
-    chrome.tabs.onAttached.addListener(this.tabRemoved)
-    chrome.tabs.onMoved.addListener(this.tabRemoved)
-    chrome.windows.onCreated.addListener(this.windowCreated)
-    chrome.windows.onRemoved.addListener(this.windowRemoved)
+    @inject(TYPES.BadgeController) private badgeController: BadgeController,
+  ) {
+    chrome.tabs.onCreated.addListener(this.tabCreated.bind(this))
+    chrome.tabs.onUpdated.addListener(this.tabUpdated.bind(this))
+    chrome.tabs.onRemoved.addListener(this.tabRemoved.bind(this))
+    chrome.tabs.onReplaced.addListener(this.tabRemoved.bind(this))
+    chrome.tabs.onDetached.addListener(this.tabRemoved.bind(this))
+    chrome.tabs.onAttached.addListener(this.tabRemoved.bind(this))
+    chrome.tabs.onMoved.addListener(this.tabRemoved.bind(this))
+    chrome.windows.onCreated.addListener(this.windowCreated.bind(this))
+    chrome.windows.onRemoved.addListener(this.windowRemoved.bind(this))
     this.badgeController.updateTabCount(this.tabData.query())
   }
 
-  async tabCreated(tab: Tab) {
+  private async tabCreated(tab: Tab) {
     const currentTabsPromise = this.tabData.query()
 
     this.saveEventToDatabase(TrackedEvent.TabOpened, currentTabsPromise, tab)
   }
 
-  async tabRemoved(tabId: number) {
+  private async tabRemoved(tabId: number) {
     return this.saveEventToDatabase(TrackedEvent.TabClosed, this.tabData.query())
   }
 
-  async tabUpdated(tabId: number, changeInfo: TabChangeInfo, tab: Tab) {
+  private async tabUpdated(tabId: number, changeInfo: TabChangeInfo, tab: Tab) {
     const currentTabsPromise = this.tabData.query()
 
     this.deduplicate(tab, currentTabsPromise)
   }
 
-  windowCreated(_window: Window, filter?: WindowEventFilter | undefined) {
+  private windowCreated(_window: Window, filter?: WindowEventFilter | undefined) {
     return this.saveEventToDatabase(TrackedEvent.WindowOpened, this.tabData.query())
   }
 
-  windowRemoved(windowId: number) {
+  private windowRemoved(windowId: number) {
     return this.saveEventToDatabase(TrackedEvent.WindowClosed, this.tabData.query())
   }
 
-  async saveEventToDatabase(
+  private async saveEventToDatabase(
     event: TrackedEvent,
     currentTabsPromise: Promise<chrome.tabs.Tab[]>,
     tab?: chrome.tabs.Tab
@@ -96,7 +82,7 @@ class TabController {
     })
   }
 
-  deduplicate(newTab: chrome.tabs.Tab, existingTabsPromise: Promise<chrome.tabs.Tab[]>) {
+  private deduplicate(newTab: chrome.tabs.Tab, existingTabsPromise: Promise<chrome.tabs.Tab[]>) {
     if (!new Experiments().tabDeduplication) {
       return
     }
@@ -105,7 +91,7 @@ class TabController {
       return
     }
 
-    existingTabsPromise.then(existingTabs => {
+    existingTabsPromise.then((existingTabs) => {
       if (existingTabs.includes(newTab)) {
         this.tabData.remove(newTab.id!)
       }
