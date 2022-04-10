e23938aeb89db0802d6ecc13baa27bc09eff350b
aljosa <aljosa.mrak@gmail>
2022-04-10 22:02:32 +0200

    Disable Google Analytics

--- src/analytics/GoogleAnalytics.ts
+++ src/analytics/GoogleAnalytics.ts
@@ -1,7 +1,5 @@
 import {Analytics, EventArgs, TimingArgs} from './Analytics'
 import {Injectable} from '@angular/core'
-import {measure} from 'measurement-protocol'
-import {IncomingMessage} from 'http'
 
 const TRACKING_ID = '${__TRACKING_ID__}'
 
@@ -9,25 +7,25 @@ const TRACKING_ID = '${__TRACKING_ID__}'
   providedIn: 'root',
 })
 class GoogleAnalytics implements Analytics {
-  async event(eventArgs: EventArgs): Promise<Response | IncomingMessage> {
-    return measure(TRACKING_ID)
-      .event(
-        eventArgs.category,
-        eventArgs.action,
-        eventArgs.label,
-        eventArgs.value,
-      )
-      .send()
+  async event(eventArgs: EventArgs) {
+    // return measure(TRACKING_ID)
+    //   .event(
+    //     eventArgs.category,
+    //     eventArgs.action,
+    //     eventArgs.label,
+    //     eventArgs.value,
+    //   )
+    //   .send()
   }
 
-  async time(timeArgs: TimingArgs): Promise<Response | IncomingMessage> {
-    return measure(TRACKING_ID)
-      .timing(timeArgs.category, timeArgs.name, timeArgs.value, timeArgs.label)
-      .send()
+  async time(timeArgs: TimingArgs) {
+    // return measure(TRACKING_ID)
+    //   .timing(timeArgs.category, timeArgs.name, timeArgs.value, timeArgs.label)
+    //   .send()
   }
 
-  async modalView(name: string): Promise<Response | IncomingMessage> {
-    return measure(TRACKING_ID).screenview(name).send()
+  async modalView(name: string) {
+    // return measure(TRACKING_ID).screenview(name).send()
   }
 }
 
