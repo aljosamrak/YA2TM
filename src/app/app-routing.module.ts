4da68a241f454d776df2194c84552fc49824fc26
aljosa <aljosa.mrak@gmail>
2022-04-17 00:13:11 +0200

    Set default routing to graphs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
-import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router'
-import {TabInsightsComponent} from './tab-insights/history/tab-insights-component.component'
-import {TabsComponent} from './tabs/tabs.component'
-import {PageNotFoundComponentComponent} from './page-not-found-component/page-not-found-component.component'
-import {AchievementsComponent} from './achievements/achievements.component'
-import {DuplicatesComponent} from './duplicates/duplicates.component'
-import {SettingsComponent} from './settings/settings.component'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AchievementsComponent } from './achievements/achievements.component'
+import { DuplicatesComponent } from './duplicates/duplicates.component'
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component'
+import { SettingsComponent } from './settings/settings.component'
+import { TabInsightsComponent } from './tab-insights/history/tab-insights-component.component'
+import { TabsComponent } from './tabs/tabs.component'
 
 const routes: Routes = [
-  {path: 'tabs', component: TabsComponent},
-  {path: 'graphs', component: TabInsightsComponent},
-  {path: 'duplicates', component: DuplicatesComponent},
-  {path: 'achievements', component: AchievementsComponent},
-  {path: 'settings', component: SettingsComponent},
-  {path: '', redirectTo: '/first-component', pathMatch: 'full'}, // redirect to `first-component`
-  {path: '**', component: PageNotFoundComponentComponent}, // Wildcard route for a 404 page
+  { path: 'tabs', component: TabsComponent },
+  { path: 'graphs', component: TabInsightsComponent },
+  { path: 'duplicates', component: DuplicatesComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: 'settings', component: SettingsComponent },
+  // Default to graphs
+  { path: '', redirectTo: '/graphs', pathMatch: 'full' },
+  // Wildcard route for a 404 page
+  { path: '**', component: PageNotFoundComponentComponent },
 ]
 
 @NgModule({
