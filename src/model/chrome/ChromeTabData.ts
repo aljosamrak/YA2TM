2ef55fc3afc73a4724e9f2e455c2f15c594c6816
aljosa <aljosa.mrak@gmail>
2022-04-20 21:22:10 +0200

    Remove obsolete code

--- src/model/chrome/ChromeTabData.ts
+++ src/model/chrome/ChromeTabData.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core'
 import { TabData } from '../TabData'
 import Tab = chrome.tabs.Tab
-import {Injectable} from '@angular/core'
 
 @Injectable({
   providedIn: 'root',
