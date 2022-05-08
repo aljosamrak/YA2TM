e940e48656860da47bbbf7c7e3bf68df958e6823
aljosa <aljosa.mrak@gmail>
2022-05-08 14:27:46 +0200

    Disable broken drill down component

--- src/app/drill-down/drill-down.component.ts
+++ src/app/drill-down/drill-down.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core'
-import { Database, TabRelation } from '../../model/Database'
-import groupBy from '../../util/utils'
+import { Database } from '../../model/Database'
 
 @Component({
   selector: 'app-drill-down',
@@ -8,19 +7,20 @@ import groupBy from '../../util/utils'
   styleUrls: ['./drill-down.component.sass'],
 })
 export class DrillDownComponent implements OnInit {
-  recordMap?: Record<string, TabRelation[]>
+  // recordMap?: Record<string, TabRelation[]>
+  recordMap?: Record<string, any[]>
 
   constructor(@Inject('Database') private database: Database) {}
 
   ngOnInit(): void {
-    this.database.getTabs().then((_data) => {
-      console.log(_data)
-      const results = groupBy(_data, (tabRelation: TabRelation) =>
-        new Date(tabRelation.createdTimestamp!).setHours(0, 0, 0, 0).toString(),
-      )
-      console.log(results)
-      this.recordMap = results
-    })
+    // this.database.getTabs().then((_data) => {
+    //   console.log(_data)
+    //   const results = groupBy(_data, (tabRelation: TabRelation) =>
+    //     new Date(tabRelation.createdTimestamp!).setHours(0, 0, 0, 0).toString(),
+    //   )
+    //   console.log(results)
+    //   this.recordMap = results
+    // })
   }
 
   toDate(timestamp: string) {
