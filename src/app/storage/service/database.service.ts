cddaee7495af845ad37ecae75291c0a8dff2d5e1
aljosa <aljosa.mrak@gmail>
2022-06-11 14:00:29 +0200

    Add analytics to the database service

--- src/app/storage/service/database.service.ts
+++ src/app/storage/service/database.service.ts
@@ -19,13 +19,35 @@ export class DatabaseService extends Dexie {
     protected analytics: AnalyticsService,
   ) {
     super(DatabaseService.DATABASE_NAME)
+
+    const start = performance.now()
+
     this.version(2).stores({
       tanEvents: 'timestamp',
       tabs: null,
     })
 
-    // opening the database
-    this.open().catch((err) => this.logger.error(err.message))
+    // Open the database
+    this.open()
+      .then(() => {
+        this.analytics.time({
+          category: 'Database',
+          name: 'Open time',
+          value: performance.now() - start,
+        })
+
+        navigator.storage.estimate().then((estimate: StorageEstimate) => {
+          if (estimate.usage && estimate.quota) {
+            analytics.event({
+              category: 'Database',
+              action: 'Open size',
+              value: (estimate?.usage / estimate?.quota) * 100,
+              label: `usage: ${estimate.usage}, quota: ${estimate.quota}`,
+            })
+          }
+        })
+      })
+      .catch((err) => this.logger.error(err.message))
   }
 
   async deleteData(): Promise<void> {
@@ -40,9 +62,22 @@ export class DatabaseService extends Dexie {
     startDate: number,
     endDate: number,
   ): Promise<EventRecord[]> {
-    return this.tanEvents
+    const startTime = performance.now()
+
+    const dataPromise = this.tanEvents
       .where('timestamp')
       .between(startDate, endDate)
       .toArray()
+
+    dataPromise.then((records) => {
+      this.analytics.time({
+        category: 'Database',
+        name: 'Query time',
+        value: performance.now() - startTime,
+        label: `Window: ${endDate - startTime}, size: ${records.length}`,
+      })
+    })
+
+    return dataPromise
   }
 }
