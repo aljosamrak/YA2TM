f26672273507ed15366c9f3a87ead164eab5cf3f
aljosa <aljosa.mrak@gmail>
2022-06-12 17:37:22 +0200

    Fix settings component test

--- src/app/settings/component/settings.component.spec.ts
+++ src/app/settings/component/settings.component.spec.ts
@@ -1,8 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ReactiveFormsModule } from '@angular/forms'
 import { LoggerTestingModule } from 'ngx-logger/testing'
+import { Observable } from 'rxjs'
 
 import { AnalyticsService } from '../../analytics/analytics.service'
+import { DatabaseService } from '../../storage/service/database.service'
+import { UserPreferences } from '../model/user-preferences'
+import { SettingsService } from '../service/settings.service'
 import { SettingsComponent } from './settings.component'
 
 describe('SettingsComponent', () => {
@@ -10,13 +14,23 @@ describe('SettingsComponent', () => {
   let fixture: ComponentFixture<SettingsComponent>
 
   const analyticsSpy = jasmine.createSpyObj('AnalyticsService', ['event'])
+  const databaseSpy = jasmine.createSpyObj('DatabaseService', [''])
+  const settingsSpy = jasmine.createSpyObj(
+    'SettingsService',
+    ['getUserPreferences'],
+    { userPreferences$: new Observable() },
+  )
 
   beforeEach(async () => {
+    settingsSpy.getUserPreferences.and.returnValue(new UserPreferences())
+
     await TestBed.configureTestingModule({
       imports: [LoggerTestingModule, ReactiveFormsModule],
       providers: [
         SettingsComponent,
         { provide: AnalyticsService, useValue: analyticsSpy },
+        { provide: DatabaseService, useValue: databaseSpy },
+        { provide: SettingsService, useValue: settingsSpy },
       ],
     }).compileComponents()
   })
