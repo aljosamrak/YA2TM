9a42890f5ccd27d101eef8736ead2b6343b151d8
aljosa <aljosa.mrak@gmail>
2022-09-24 23:49:49 +0200

    Display drill down tabs by opened date

--- src/app/drill-down/drill-down.component.ts
+++ src/app/drill-down/drill-down.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+import { OpenTab } from '../storage/model/OpenTab'
+import { DatabaseService } from '../storage/service/database.service'
+import groupBy from '../util/utils'
 
 @Component({
   selector: 'app-drill-down',
   templateUrl: './drill-down.component.html',
   styleUrls: ['./drill-down.component.sass'],
 })
-export class DrillDownComponent {
-  recordMap?: Record<string, any[]>
+export class DrillDownComponent implements OnInit {
+  recordMap?: Record<string, OpenTab[]>
+
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit(): void {
+    const currentTime = new Date().getTime()
+    this.databaseService
+      .getOpenTabs()
+      .then((_data) => {
+        this.recordMap = groupBy(_data, (openTab: OpenTab) =>
+          new Date(openTab?.createdTimestamp ?? currentTime)
+            .setHours(0, 0, 0, 0)
+            .toString(),
+        )
+      })
+      .catch((err) => console.error(err.message))
+  }
 
   toDate(timestamp: string) {
     return new Date(parseInt(timestamp, 10)).toDateString()
